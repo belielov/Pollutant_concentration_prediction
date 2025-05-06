f861a1afd133fe4d9a78b8bd218ba2a1f7e6aa75
THice <xiuhuangxiang@gmail.com>
2025-05-06 20:31:08 +0800

    Modify python file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Pollutant_concentration_prediction
+本项目使用的数据集为 GF5A.xlsx ，共有330列，112行。每一行代表一个采样点，每一列代表一个波段。
+
+源代码位于 src 文件夹，训练结果以图片的形式存储在 imgs 文件夹下。
--- src/Pollutant_concentration_prediction.py
+++ src/Pollutant_concentration_prediction.py
@@ -1,5 +1,25 @@
+import os.path
+
+import matplotlib.pyplot as plt
+
+# 中文字体配置
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
 
 # ---------------- 数据导入 ----------------
+# 获取当前脚本文件的绝对路径并提取所在目录路径
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# 获取项目根目录路径
+project_dir = os.path.dirname(current_dir)
+# 构建数据集所在路径
+dataset_path = os.path.join(project_dir, 'dataset', 'GF5A.xlsx')
+
+# # 测试：打印路径
+# print(f'''
+# 当前脚本所在目录为：{current_dir}
+# 项目路径为：{project_dir}
+# 数据集所在路径为：{dataset_path}
+# ''')
 
 # ---------------- 数据预处理 ----------------
 
